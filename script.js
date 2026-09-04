ea0839115f6de3404252965b701388007c2b30ef
taj728 <tajaleemsait20@gmail.com>
2026-09-04 13:09:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let level =0;
+let temp =document.getElementById("level");
+let body = document.querySelector("body");
+while(temp){
+	level++;
+	temp=temp.parentElement;
+}
+let p= document.createElement("p");
+p.textContent=`The level of the element is: ${level}`
+body.append(p);
